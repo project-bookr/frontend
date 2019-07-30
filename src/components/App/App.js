17315cc75efcfd29208d0999ced325e4e15a421e
jtwray <tuckerwray84@gmail.com>
2019-07-29 21:07:28 -0400

    renames components file

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {BrowserRouter as Router, Route, } from "react-router-dom";
+import Login from "../Login/Login.js";
+import PrivateRoute from "../components/PrivateRoute/PrivateRoute.js";
+import Register from "../Register/Register.js";
+import Dashboard from "../Dashboard/Dashboard.js"
+import "./App.css";
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/register" component={Register} />
+				<PrivateRoute exact path="/dashboard" component={Dashboard} />
+				<Route exact path="/login" component={Login} />
+			
+			</div>
+		</Router>
+	);
+}
+
+export default App;
