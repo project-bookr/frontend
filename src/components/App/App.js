e841255d042f6126b0dce33ce6c5b631a07c969e
jtwray <tuckerwray84@gmail.com>
2019-07-30 10:45:07 -0400

    App component: defines new route'/' as MarketingPage/Creates component MarketPage.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter as Router, Route, } from "react-router-dom";
 import Login from "../Login/Login.js";
 import PrivateRoute from "../PrivateRoute/PrivateRoute.js"
 import Register from "../Register/Register.js";
-import Dashboard from "../Dashboard/Dashboard.js"
+import Dashboard from "../Dashboard/Dashboard.js";
+import MarketingPage from "../MarketingPage/MarketingPage"
 
-function App() {
+const App=()=> {
 	return (
 		<Router>
 			<div className="App">
 				<Route exact path="/register" component={Register} />
+				<Route exact path="/" component={MarketingPage}/>
 				<PrivateRoute exact path="/dashboard" component={Dashboard} />
 				<Route exact path="/login" component={Login} />
 			
---
+++ src/components/MarketingPage/MarketingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+const MarketingPage=()=> {
+	return (
+        <strong>
+        Bookr
+        </strong>
+	);
+}
+
+export default MarketingPage;
